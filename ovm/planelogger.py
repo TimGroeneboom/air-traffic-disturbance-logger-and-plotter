b71e622de3fddd901ec8be70e70ab5ba7b519c06
Tim Groeneboom <tim@timgroeneboom.com>
2023-02-21 20:30:48 +0100

    - moved planelogger and stateplotter to ovm dir - added nuisance_check.py - added environment.json containing environment configuration

---
+++ ovm/planelogger.py
@@ -0,0 +1,70 @@
+import logging
+from datetime import datetime
+from opensky_api import OpenSkyApi
+from ovm.stateplotter import StatePlotter
+from pymongo import MongoClient
+from dataclasses import dataclass
+
+@dataclass
+class PlotOptions:
+    plot: bool
+    tilezoom: int
+    filename: str
+
+
+class PlaneLogger:
+    # parameterized constructor
+    def __init__(self, environment):
+        # Set environment
+        self.environment = environment
+
+        # Create MongoDB client
+        self.mongo_client = MongoClient(environment['mongodb_config']['host'],
+                                        environment['mongodb_config']['port'])
+
+        # Create plotter
+        self.state_plotter = StatePlotter()
+
+        # Connect with opensky network
+        self.opensky_api = OpenSkyApi(environment['opensky_credentials']['username'],
+                                      environment['opensky_credentials']['password'])
+
+    def prepare_log(self, message: str):
+        return self.__class__.__name__ + ': ' + message
+
+    def log(self, bbox: tuple, plot_options: PlotOptions = None):
+        # Obtain current states
+        logging.info(self.prepare_log('Obtaining states from opensky network'))
+        state_collection = self.opensky_api.get_states(bbox=bbox)
+
+        # Create states list with interesting data and store list in mongo db with timestamp as key value
+        logging.info(self.prepare_log('Storing %i states in database' % len(state_collection.states)))
+        key = int(datetime.now().strftime("%Y%m%d%H%M%S"))
+        db_config = self.environment['mongodb_config']
+        db_states = self.mongo_client[db_config['database']][db_config['collection']]
+        states = []
+        for state in state_collection.states:
+            state_object = {
+                "longitude": state.longitude,
+                "latitude": state.latitude,
+                "icao24": state.icao24,
+                "callsign": state.callsign,
+                "on_ground": state.on_ground,
+                "baro_altitude": state.baro_altitude,
+                "geo_altitude": state.geo_altitude,
+                "origin_country": state.origin_country,
+                "velocity": state.velocity
+            }
+            states.append(state_object)
+        result = db_states.insert_one({
+            'Time': key,
+            'States': states
+        })
+
+        # Plot if necessary
+        if plot_options is not None and plot_options.plot:
+            logging.info(self.prepare_log('Creating plot'))
+            self.state_plotter.plot(states,
+                                    bbox=bbox,
+                                    tile_zoom=plot_options.tilezoom,
+                                    filename=plot_options.filename)
