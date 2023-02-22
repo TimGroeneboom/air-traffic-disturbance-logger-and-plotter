89cad108747d380330842911f18e1bd31983e1f2
Tim Groeneboom <tim@timgroeneboom.com>
2023-02-22 20:38:58 +0100

    - added complainant data class - better plotting of disturbances

--- nuisance_check.py
+++ nuisance_check.py
@@ -1,193 +1,208 @@
 # Import the python libraries
 import collections
-import datetime
-import math
-
-import pymongo
 import pymongo as pymongo
 from pymongo import MongoClient
 import geopy.distance
 from ovm import environment
+from ovm.complainant import Complainant
 from ovm.disturbanceperiod import DisturbancePeriod
 from ovm.plotter import Plotter
 from ovm.utils import *
 
-# Nuisance parameters
+if __name__ == '__main__':
+    # TODO: make this real, fetch complainants from database
+    complainants = []
+    complainants.append(Complainant(user='CodeFlow',
+                                    # Amsterdamse Bos
+                                     #origin=(52.311502, 4.827680),
+                                    # Assendelft
+                                    #origin=(52.469640, 4.721354),
+                                    # Christoffelkruidstraat
+                                    origin=(52.396172234741506, 4.905621078252285),
+                                    radius=1250,
+                                    altitude=1000,
+                                    occurrences=4,
+                                    timeframe=60))
 
-# Christoffelkruidstraat
-origin = (52.396172234741506, 4.905621078252285)
-# Amsterdamse Bos
-#origin = (52.311502, 4.827680)
-# Assendelft
-#origin = (52.469640, 4.721354)
-radius = 1250
-altitude = 1000
-occurrences = 4
-timeframe = 60
+    # Load environment
+    environment = environment.load_environment('environment.json')
 
-# Load environment
-environment = environment.load_environment('environment.json')
+    # Choose the appropriate client
+    client = MongoClient(environment.mongodb_config.host, environment.mongodb_config.port)
 
-# Choose the appropriate client
-client = MongoClient(environment.mongodb_config.host, environment.mongodb_config.port)
-
-# Ignore certain callsigns
-ignore_callsigns = [
-    'LIFELN1'
-]
-
-
-def ignore_callsign(callsign: str):
-    for other in ignore_callsigns:
-        if callsign_compare(callsign, other):
-            return True
-
-    return False
+    # Ignore certain callsigns
+    ignore_callsigns = [
+        'LIFELN1'
+    ]
 
 
-disturbance_callsigns: dict = ***REMOVED******REMOVED***
-disturbance_periods = []
-last_timestamp: datetime
-disturbance_begin: datetime
-last_disturbance: datetime
-disturbance_hits = 0
-in_disturbance = False
-states_collection = client[environment.mongodb_config.database][environment.mongodb_config.collection]
-cursor = states_collection.find(***REMOVED******REMOVED***)
+    def ignore_callsign(callsign: str):
+        for other in ignore_callsigns:
+            if callsign_compare(callsign, other):
+                return True
 
-# Iterate through states
-for document in cursor:
-    timestamp_int = document['Time']
-    timestamp = convert_int_to_datetime(timestamp_int)
-    states = document['States']
-    disturbance_in_this_timestamp = False
+        return False
 
-    for state in states:
-        # Get callsign
-        callsign = state['callsign']
 
-        # Ignore grounded planes
-        if state['baro_altitude'] is None:
-            continue
+    for complainant in complainants:
+        disturbances: dict = ***REMOVED******REMOVED***
+        disturbance_periods = []
+        last_timestamp: datetime
+        disturbance_begin: datetime
+        last_disturbance: datetime
+        disturbance_hits = 0
+        in_disturbance = False
+        states_collection = client[environment.mongodb_config.database][environment.mongodb_config.collection]
+        cursor = states_collection.find(***REMOVED******REMOVED***)
+        total_altitude = 0
 
-        # Ignore specified callsigns
-        if ignore_callsign(callsign):
-            continue
+        # Iterate through states
+        for document in cursor:
+            timestamp_int = document['Time']
+            timestamp = convert_int_to_datetime(timestamp_int)
+            states = document['States']
+            disturbance_in_this_timestamp = False
 
-        # Check if altitude is lower than altitude and if distance is within specified radius
-        if state['baro_altitude'] < altitude:
-            coord = (state['latitude'], state['longitude'])
-            distance = geopy.distance.distance(origin, coord).meters
-            if distance < radius:
-                disturbance_hits += 1
+            for state in states:
+                # Get callsign
+                callsign = state['callsign']
 
-                # Check if there already is a disturbance in this timeframe, otherwise create a new disturbance
-                disturbance_in_this_timestamp = True
-                if not in_disturbance:
-                    in_disturbance = True
-                    disturbance_begin = timestamp
-                    last_disturbance = timestamp
-                else:
-                    diff_since_begin = timestamp - disturbance_begin
+                # Ignore grounded planes
+                if state['baro_altitude'] is None:
+                    continue
+
+                # Ignore specified callsigns
+                if ignore_callsign(callsign):
+                    continue
+
+                # Check if altitude is lower than altitude and if distance is within specified radius
+                if state['baro_altitude'] < complainant.altitude:
+                    coord = (state['latitude'], state['longitude'])
+                    distance = geopy.distance.distance(complainant.origin, coord).meters
+                    if distance < complainant.radius:
+                        disturbance_hits += 1
+                        total_altitude += state['baro_altitude']
+
+                        # Check if there already is a disturbance in this timeframe, otherwise create a new disturbance
+                        disturbance_in_this_timestamp = True
+                        if not in_disturbance:
+                            in_disturbance = True
+                            disturbance_begin = timestamp
+                            last_disturbance = timestamp
+                        else:
+                            diff_since_begin = timestamp - disturbance_begin
+                            diff_since_last = timestamp - last_disturbance
+                            last_disturbance = timestamp
+
+                        # if callsign is not already logged for this disturbance, do it now
+                        if callsign not in disturbances.keys():
+                            disturbances[callsign] = timestamp_int
+
+            # Check if disturbance has ended and if we need to generate a complaint within set parameters
+            if not disturbance_in_this_timestamp:
+                if in_disturbance:
                     diff_since_last = timestamp - last_disturbance
-                    last_disturbance = timestamp
+                    disturbance_duration = last_disturbance - disturbance_begin
+                    diff_since_begin = timestamp - disturbance_begin
+                    if (diff_since_last.seconds / 60) >= complainant.timeframe:
+                        if disturbance_hits >= complainant.occurrences:
+                            disturbance_period = DisturbancePeriod(complainant=complainant,
+                                                                   disturbances=disturbances.copy(),
+                                                                   begin=disturbance_begin,
+                                                                   end=last_disturbance,
+                                                                   hits=disturbance_hits,
+                                                                   average_altitude=total_altitude / disturbance_hits)
+                            disturbance_periods.append(disturbance_period)
 
-                # if callsign is not already logged for this disturbance, do it now
-                if callsign not in disturbance_callsigns.keys():
-                    disturbance_callsigns[callsign] = timestamp_int
+                        in_disturbance = False
+                        disturbance_begin = None
+                        last_disturbance = None
+                        disturbance_hits = 0
+                        total_altitude = 0
+                        disturbances = ***REMOVED******REMOVED***
 
-    # Check if disturbance has ended and if we need to generate a complaint within set parameters
-    if not disturbance_in_this_timestamp:
+            last_timestamp = timestamp
+
+        # Handle disturbance if we finished in disturbance state
         if in_disturbance:
             diff_since_last = timestamp - last_disturbance
             disturbance_duration = last_disturbance - disturbance_begin
             diff_since_begin = timestamp - disturbance_begin
-            if (diff_since_last.seconds / 60) >= timeframe:
-                if disturbance_hits >= occurrences:
-                    disturbance_complaint = DisturbancePeriod(origin,
-                                                              radius,
-                                                              disturbance_callsigns.copy(),
-                                                              disturbance_begin,
-                                                              last_disturbance,
-                                                              disturbance_hits)
-                    disturbance_periods.append(disturbance_complaint)
 
-                in_disturbance = False
-                disturbance_begin = None
-                last_disturbance = None
-                disturbance_hits = 0
-                disturbance_callsigns = ***REMOVED******REMOVED***
+            if disturbance_hits >= complainant.occurrences:
+                if (disturbance_duration.seconds / 60) > complainant.timeframe:
+                    disturbance_period = DisturbancePeriod(complainant=complainant,
+                                                           disturbances=disturbances.copy(),
+                                                           begin=disturbance_begin,
+                                                           end=last_disturbance,
+                                                           hits=disturbance_hits,
+                                                           average_altitude=total_altitude / disturbance_hits)
+                    disturbance_periods.append(disturbance_period)
 
-    last_timestamp = timestamp
+        # Calc trajectories for generate complaints for callsigns
+        index = 0
+        for disturbance_period in disturbance_periods:
+            # Calc disturbance duration
+            disturbance_duration = disturbance_period.end - disturbance_period.begin
+            print('Disturbance detected. %i hits and a total duration of %i minutes\n'
+                  'Disturbance began at %s and ended at %s' %
+                  (disturbance_period.hits, (disturbance_duration.seconds / 60),
+                   disturbance_period.begin.__str__(), disturbance_period.end.__str__()))
 
-# Handle disturbance if we finished in disturbance state
-if in_disturbance:
-    diff_since_last = timestamp - last_disturbance
-    disturbance_duration = last_disturbance - disturbance_begin
-    diff_since_begin = timestamp - disturbance_begin
+            # Create trajectories for complaint
+            for callsign, datetime_int in disturbance_period.disturbances.items():
+                # Gather states before and after this entry to plot a trajectory for callsign
+                dictionary = ***REMOVED******REMOVED***
+                items_before = states_collection.find(***REMOVED***'Time': ***REMOVED***'$gte': datetime_int***REMOVED******REMOVED***).limit(4)
+                items_after = states_collection.find(***REMOVED***'Time': ***REMOVED***'$lte': datetime_int***REMOVED******REMOVED***).sort(
+                    [('Time', pymongo.DESCENDING)]).limit(4)
+                for doc in items_before:
+                    timestamp_int = doc['Time']
+                    if timestamp_int not in dictionary.keys():
+                        dictionary[timestamp_int] = doc['States']
+                for doc in items_after:
+                    timestamp_int = doc['Time']
+                    if timestamp_int not in dictionary.keys():
+                        dictionary[timestamp_int] = doc['States']
 
-    if disturbance_hits >= occurrences:
-        if (disturbance_duration.seconds / 60) > timeframe:
-            disturbance_complaint = DisturbancePeriod(origin,
-                                                      radius,
-                                                      disturbance_callsigns.copy(),
-                                                      disturbance_begin,
-                                                      last_disturbance,
-                                                      disturbance_hits)
-            disturbance_periods.append(disturbance_complaint)
+                # Order found states and create the trajectory of disturbance callsign
+                trajectory = []
+                ordered_dict = collections.OrderedDict(sorted(dictionary.items()))
+                for key, value in ordered_dict.items():
+                    for state in value:
+                        if state['callsign'] == callsign:
+                            coord = (state['longitude'], state['latitude'])
+                            trajectory.append(coord)
 
+                # Add it to the trajectories of this complaint
+                disturbance_period.trajectories[callsign] = trajectory
 
-# Calc trajectories for generate complaints for callsigns
-index = 0
-for disturbance_period in disturbance_periods:
-    # Calc disturbance duration
-    disturbance_duration = disturbance_period.end - disturbance_period.begin
-    print('Disturbance detected. %i hits and a total duration of %i minutes\n'
-          'Disturbance began at %s and ended at %s' %
-          (disturbance_period.hits, (disturbance_duration.seconds / 60),
-           disturbance_period.begin.__str__(), disturbance_period.end.__str__()))
+            # Set the bounding box for our area of interest, add an extra meters/padding for a better view of
+            # trajectories
+            r_earth = 6378
+            padding = 1000
+            lat_min = disturbance_period.complainant.origin[0] - (
+                        ((complainant.radius + padding) / 1000.0) / r_earth) * (
+                              180.0 / math.pi)
+            lon_min = disturbance_period.complainant.origin[1] - (
+                        ((complainant.radius + padding) / 1000.0) / r_earth) * (
+                              180.0 / math.pi) / math.cos(disturbance_period.complainant.origin[0] * math.pi / 180.0)
+            lat_max = disturbance_period.complainant.origin[0] + (
+                        ((complainant.radius + padding) / 1000.0) / r_earth) * (
+                              180.0 / math.pi)
+            lon_max = disturbance_period.complainant.origin[1] + (
+                        ((complainant.radius + padding) / 1000.0) / r_earth) * (
+                              180.0 / math.pi) / math.cos(disturbance_period.complainant.origin[0] * math.pi / 180.0)
 
-    # Create trajectories for complaint
-    for callsign, datetime_int in disturbance_period.disturbance_callsigns.items():
-        # Gather states before and after this entry to plot a trajectory for callsign
-        dictionary = ***REMOVED******REMOVED***
-        items_before = states_collection.find(***REMOVED***'Time': ***REMOVED***'$gte': datetime_int***REMOVED******REMOVED***).limit(4)
-        items_after = states_collection.find(***REMOVED***'Time': ***REMOVED***'$lte': datetime_int***REMOVED******REMOVED***).sort(
-            [('Time', pymongo.DESCENDING)]).limit(4)
-        for doc in items_before:
-            timestamp_int = doc['Time']
-            if timestamp_int not in dictionary.keys():
-                dictionary[timestamp_int] = doc['States']
-        for doc in items_after:
-            timestamp_int = doc['Time']
-            if timestamp_int not in dictionary.keys():
-                dictionary[timestamp_int] = doc['States']
+            # Make plot of all callsign trajectories
+            index += 1
+            plotter = Plotter()
+            plotter.plot_trajectories(bbox=(lat_min, lat_max, lon_min, lon_max),
+                                      disturbance_period=disturbance_period,
+                                      tile_zoom=14,
+                                      figsize=(10, 10),
+                                      filename=('%s%s%i.jpg' % (disturbance_period.complainant.user,
+                                                                disturbance_period.begin.strftime("%Y%m%d%H%M%S"),
+                                                                index)))
 
-        # Order found states and create the trajectory of disturbance callsign
-        trajectory = []
-        ordered_dict = collections.OrderedDict(sorted(dictionary.items()))
-        for key, value in ordered_dict.items():
-            for state in value:
-                if state['callsign'] == callsign:
-                    coord = (state['longitude'], state['latitude'])
-                    trajectory.append(coord)
-
-        # Add it to the trajectories of this complaint
-        disturbance_period.trajectories[callsign] = trajectory
-
-    # Set the bounding box for our area of interest
-    r_earth = 6378
-    padding = 1000
-    lat_min = disturbance_period.coord[0] - (((radius + padding) / 1000.0) / r_earth) * (180.0 / math.pi)
-    lon_min = disturbance_period.coord[1] - (((radius + padding) / 1000.0) / r_earth) * (180.0 / math.pi) / math.cos(disturbance_period.coord[0] * math.pi / 180.0)
-    lat_max = disturbance_period.coord[0] + (((radius + padding) / 1000.0) / r_earth) * (180.0 / math.pi)
-    lon_max = disturbance_period.coord[1] + (((radius + padding) / 1000.0) / r_earth) * (180.0 / math.pi) / math.cos(disturbance_period.coord[0] * math.pi / 180.0)
-
-    # Make plot of all callsign trajectories
-    index += 1
-    plotter = Plotter()
-    plotter.plot_trajectories(bbox=(lat_min, lat_max, lon_min, lon_max),
-                              disturbance_period=disturbance_period,
-                              tile_zoom=14,
-                              figsize=(10,10),
-                              filename=('complaint%i.jpg' % index))
+    exit(0)
--- ovm/plotter.py
+++ ovm/plotter.py
@@ -2,7 +2,7 @@ import json
 import math
 import pandas as pd
 import geopandas as gpd
-from shapely import LineString
+from shapely import LineString, Point
 from shapely.geometry import Polygon
 from pyproj import CRS, Proj, transform, Transformer
 import contextily as ctx
@@ -66,7 +66,7 @@ class Plotter:
         gdf_3857 = gdf.to_crs(epsg=3857)  # web mercator
         f, ax = plt.subplots(figsize=figsize)
         bounds_3857.plot(ax=ax, alpha=0.0, edgecolor="k")
-        gdf_3857.plot_states(ax=ax, alpha=0.8, edgecolor="k")
+        gdf_3857.plot(ax=ax, alpha=0.8, edgecolor="k")
         ctx.add_basemap(ax, zoom=tile_zoom)
         ax.set_axis_off()
         ax.get_xaxis().set_visible(False)
@@ -80,66 +80,65 @@ class Plotter:
                           figsize: tuple = (15, 15),
                           tile_zoom: int = 8,
                           filename: str = "traffic.png"):
-            # define lat lon bounding box
-            lat_min = bbox[0]
-            lat_max = bbox[1]
-            lon_min = bbox[2]
-            lon_max = bbox[3]
+        # define lat lon bounding box
+        lat_min = bbox[0]
+        lat_max = bbox[1]
+        lon_min = bbox[2]
+        lon_max = bbox[3]
 
-            linestrings = ***REMOVED******REMOVED***
-            for key, value in disturbance_period.trajectories.items():
-                if len(value) >= 2:
-                    linestring: LineString = LineString(value)
-                    linestrings[key] = linestring
+        linestrings = ***REMOVED******REMOVED***
+        for key, value in disturbance_period.trajectories.items():
+            if len(value) >= 2:
+                linestring: LineString = LineString(value)
+                linestrings[key] = linestring
 
-            data = []
-            for key, value in linestrings.items():
-                data.append([key, value])
+        data = []
+        for key, value in linestrings.items():
+            data.append([key, value])
 
-            # create data frame
-            df = pd.DataFrame(data, columns=['callsign', 'LineString_obj'])
-            gdf = gpd.GeoDataFrame(
-                df,
-                geometry='LineString_obj',
-                crs=CRS("EPSG:4326"),  # WGS84
-            )
-            bounds = gpd.GeoDataFrame(geometry=[Polygon([[lon_min, lat_min],
-                                                         [lon_min, lat_max],
-                                                         [lon_max, lat_max],
-                                                         [lon_max, lat_min]])],
-                                      crs="EPSG:4326")
-            # perform spatial join
-            gdf = gpd.sjoin(gdf, bounds)
+        # create data frame
+        df = pd.DataFrame(data, columns=['callsign', 'LineString_obj'])
+        gdf = gpd.GeoDataFrame(
+            df,
+            geometry='LineString_obj',
+            crs=CRS("EPSG:4326"),  # WGS84
+        )
+        bounds = gpd.GeoDataFrame(geometry=[Polygon([[lon_min, lat_min],
+                                                     [lon_min, lat_max],
+                                                     [lon_max, lat_max],
+                                                     [lon_max, lat_min]])],
+                                  crs="EPSG:4326")
+        center = gpd.GeoDataFrame(geometry=[Point([disturbance_period.complainant.origin[1],
+                                                   disturbance_period.complainant.origin[0]])],
+                                  crs="EPSG:4326")
+        # perform spatial join
+        gdf = gpd.sjoin(gdf, bounds)
 
-            # plot data on map
-            bounds_3857 = bounds.to_crs(epsg=3857)  # web mercator
-            gdf_3857 = gdf.to_crs(epsg=3857)  # web mercator
-            f, ax = plt.subplots(figsize=figsize)
-            bounds_3857.plot(ax=ax, alpha=0.0, edgecolor="blue")
-            gdf_3857.plot(ax=ax, alpha=0.6, edgecolor="red", column='callsign')
-            min_3857 = convert_epsg4326_to_epsg3857(lon_min, lat_min)
-            max_3857 = convert_epsg4326_to_epsg3857(lon_max, lat_max)
-            ax.set_xlim(min_3857[0], max_3857[0])
-            ax.set_ylim(min_3857[1], max_3857[1])
-            ctx.add_basemap(ax, zoom=tile_zoom)
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-            ax.text(0.05, 0.10,
-                    'location: [%f, %f]\nperiod: %s - %s\nflights: %i' %
-                    (disturbance_period.coord[0], disturbance_period.coord[1],
-                     disturbance_period.begin.__str__(), disturbance_period.end.__str__(),
-                     disturbance_period.hits),
-                    verticalalignment='bottom', horizontalalignment='left',
-                    transform=ax.transAxes,
-                    color='black', fontsize=15,
-                    bbox=***REMOVED***'facecolor': 'white', 'alpha': 1, 'pad': 10***REMOVED***)
-            plt.savefig(filename, bbox_inches="tight", pad_inches=-0.1)
-            plt.close()
-
-
-
-
-
-
-
-
+        # plot data on map
+        bounds_3857 = bounds.to_crs(epsg=3857)  # web mercator
+        gdf_3857 = gdf.to_crs(epsg=3857)  # web mercator
+        center_3857 = center.to_crs(epsg=3857)  # web mercator
+        f, ax = plt.subplots(figsize=figsize)
+        center_3857.plot(ax=ax, alpha=0.7, edgecolor="blue")
+        bounds_3857.plot(ax=ax, alpha=0.0, edgecolor="blue")
+        gdf_3857.plot(ax=ax, alpha=0.4, edgecolor="red", column='callsign')
+        min_3857 = convert_epsg4326_to_epsg3857(lon_min, lat_min)
+        max_3857 = convert_epsg4326_to_epsg3857(lon_max, lat_max)
+        ax.set_xlim(min_3857[0], max_3857[0])
+        ax.set_ylim(min_3857[1], max_3857[1])
+        ctx.add_basemap(ax, zoom=tile_zoom)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.text(0.05, 0.10,
+                '%s\nlocation: [%f, %f]\nperiod: [%s, %s]\nflights: %i\naverage altitude: %im' %
+                (disturbance_period.complainant.user,
+                 disturbance_period.complainant.origin[0], disturbance_period.complainant.origin[1],
+                 disturbance_period.begin.__str__(), disturbance_period.end.__str__(),
+                 len(disturbance_period.disturbances.items()), disturbance_period.averate_altitude),
+                verticalalignment='bottom', horizontalalignment='left',
+                transform=ax.transAxes,
+                color='black', fontsize=15,
+                bbox=***REMOVED***'facecolor': 'white', 'alpha': 1, 'pad': 10***REMOVED***)
+        ax.add_patch(plt.Circle((0.5, 0.5), 0.2, color='red', alpha=1.0))
+        plt.savefig(filename, bbox_inches="tight", pad_inches=-0.1)
+        plt.close()
