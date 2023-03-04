a4f3328c1a2b229a02c460558e5e94f0e16868de
codeflow <codeflow@emby.local>
2023-03-04 12:12:42 +0100

    resolved

--- flaskr/environment.py
+++ flaskr/environment.py
@@ -1,6 +1,6 @@
-# pro6pp configuration
-PRO6PP_AUTH_KEY = '<AUTH-KEY>'
-PRO6PP_API_URL = 'https://api.pro6pp.nl/v2/autocomplete/nl'
-
 # API configuration
 MAX_WORKERS = 8
+
+PRO6PP_AUTH_KEY = '64NT3jnwCVt1guuI'
+PRO6PP_API_URL = 'https://api.pro6pp.nl/v2/autocomplete/nl'
+
