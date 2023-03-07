28aee42cccf05b018253904a2659227e850375d8
Tim Groeneboom <tim@timgroeneboom.com>
2023-03-07 15:02:37 +0100

    - fixed minor issues in jobqueue.py - updated README.md - bumped version to 1.0

--- flaskr/environment.py
+++ flaskr/environment.py
@@ -1,5 +1,5 @@
 # pro6pp configuration
-PRO6PP_AUTH_KEY = '64NT3jnwCVt1guuI'
+PRO6PP_AUTH_KEY = '<AUTH-KEY>'
 PRO6PP_API_AUTO_COMPLETE_URL = 'https://api.pro6pp.nl/v2/autocomplete/nl'
 PRO6PP_API_AUTO_LOCATOR_URL = 'https://api.pro6pp.nl/v2/locator/nl'
 
