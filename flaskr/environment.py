58034ec62c0cbcd398399d525a2d1f0d9c88a516
Tim Groeneboom <tim@timgroeneboom.com>
2023-03-29 12:14:38 +0200

    - adjusted documentation and readme

--- flaskr/environment.py
+++ flaskr/environment.py
@@ -3,10 +3,6 @@ PRO6PP_AUTH_KEY = '<PRO6PP_AUTH_KEY>'
 PRO6PP_API_AUTO_COMPLETE_URL = 'https://api.pro6pp.nl/v2/autocomplete/nl'
 PRO6PP_API_AUTO_LOCATOR_URL = 'https://api.pro6pp.nl/v2/locator/nl'
 
-# API configuration
-MAX_WORKERS = 8
-MAX_SIZE_JOB_QUEUE = 1000
-
 # lat lon address cache
 LATLON_CACHE_EXPIRATION_DAYS = 180
 
